2b8303e6df55af78b8590ea0d4e16dfe3c0d855f
plaksivayatryapka <boiler00@mail.ru>
2017-02-22 12:40:38 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,6 +10,22 @@ def send_starts(players) :
             TelegramBot.sendMessage(values['id'], "You're com")
         if values['doc'] == 1 :
             TelegramBot.sendMessage(values['id'], "You're doc")
+        for key, values in players.items() :
+            mafs = []
+            coms = []
+            goods = []
+            if values['maf'] == 1 :
+                mafs.append(key)
+            elif values['com'] == 1 :
+                coms.append(key)
+            else:
+                goods.append(key)
+        mafs_acquaintance (mafs, coms, goods) # znakomstvo mafov
+            
+def mafs_acquaintance (players, mafs) :
+    
+    for maf in mafs :
+        TelegramBot.sendMessage(players[maf], mafs)
 
 def incoming(update_id, players) :
     users_voted = list()
@@ -25,77 +41,75 @@ def incoming(update_id, players) :
     print players
     return players, update_id
 
-def parse_incoming(users_voted, text_voted, phonebook, places, players) :
-    for user in users_voted :
-        if users_voted[i] == phonebook.get('slavik') :
-            users_voted[i] = places.get('slavik')
-        elif users_voted[i] == phonebook.get('polya') :
-            users_voted[i] = places.get('polya')
+def parse_incoming(users_voted, text_voted, players) :
     i = 0
     for user in users_voted :
-        if text_voted[i][0] == 'p' or text_voted[i][0] == 'P':
-            players[user][3] = int(text_voted[i][1])
-        else :
-            players[user][2] = int(text_voted[i])
+        for key, values in players.items() :
+            if values['id'] == user :
+                users_voted[i] = key
+                if text_voted[i][0] == 'p' or text_voted[i][0] == 'P':
+                    players[key]['check_maf'] = int(text_voted[i][1])                                
+                else :
+                    players[key]['vote_kill'] = int(text_voted[i][1])
         i = i + 1
+
     return players
 
-def check_mafs_murder (players, mafs, comissaire_access) :
-    votes_mafs = list()
+def check_mafs_murder (players, mafs, comissaire_access) : 
     for maf in mafs :
-        if players[maf][1] != 0 : # if alive
-            votes_mafs.append(players[maf][2]) # get mafs' votes to one array
+        if players[maf]['alive'] != 0 :
+            votes_mafs.append(players[maf]['vote_kill']) # get mafs' votes to one array
             
     if len(set(votes_mafs)) == 1 and set(votes_mafs) != set([None]): # if set consists of one element -> all mafs voted for one person -> kill him
         killed = votes_mafs[0] # variable for number of killed user
-        players[killed][1] = 0
+        players[killed]['alive'] = 0
         comissaire_access = True
-        TelegramBot.sendMessage(players[killed][0], "you're dead!")
+        TelegramBot.sendMessage(players[killed]['id'], "you're dead!")
     return players, comissaire_access
         
 def comissaire_check (players, coms, mafs, comissaire_access) :
     if comissaire_access == True :
         for com in coms :
-            if players[com][1] != 0 :
-                if players[com][3] in mafs :
-                    TelegramBot.sendMessage(players[com][0], 'maf')
+            if players[com]['alive'] != 0 :
+                if players[com]['check_maf'] in mafs :
+                    TelegramBot.sendMessage(players[com]['id'], 'maf')
                     comissaire_access = False
-                    players[com][3] = None
-                elif (players[com][3] not in mafs) and (players[com][3] != None) :
-                    TelegramBot.sendMessage(players[com][0], 'not a maf')
+                    players[com]['check_maf'] = None
+                elif (players[com]['check_maf'] not in mafs) and (players[com]['check_maf'] != None) :
+                    TelegramBot.sendMessage(players[com]['id'], 'not a maf')
                     comissaire_access = False
-                    players[com][3] = None
+                    players[com]['check_maf'] = None
     return comissaire_access
             
 def check_goods_murder (players, goods) :
     votes_goods = list()
     for good in goods :
-        if players[good][1] != 0 :
-            votes_goods.append(players[good][2])
+        if players[good]['alive'] != 0 :
+            votes_goods.append(players[good]['vote_kill'])
     if len(set(votes_goods)) == 1 and set(votes_goods) != set([None]):
         killed = votes_goods[0]
-        players[killed][1] = 0
-        TelegramBot.sendMessage(players[killed][0], "you're dead!")
+        players[killed]['alive'] = 0
+        TelegramBot.sendMessage(players[killed]['id'], "you're dead!")
     return players
 
 def check_alive (players, mafs, goods) :
     alive_mafs = 0
     alive_goods = 0
     for maf in mafs :
-        if players[maf][1] == 1 :
+        if players[maf]['alive'] != 0 :
             alive_mafs = alive_mafs + 1
         if alive_mafs == 0 : # if no mafs alive send message
             for good in goods :
-                TelegramBot.sendMessage(players[good][0], 'mafs are dead!')
+                TelegramBot.sendMessage(players[good]['id'], 'mafs are dead!')
                 exit()
     for good in goods :
-        if players[good][1] == 1 :
+        if players[good]['alive'] != 0 :
             alive_goods = alive_goods + 1
         if alive_goods == 0 : # if no mafs alive send message
             for maf in mafs :
-                TelegramBot.sendMessage(players[maf][0], 'goods are dead!')
+                TelegramBot.sendMessage(players[maf]['id'], 'goods are dead!')
                 exit()
-'''
+
 def game(players):
     
     import telepot
@@ -109,13 +123,13 @@ def game(players):
     onstart_update = TelegramBot.getUpdates()
     update_id = int(onstart_update[-1].get('update_id')) + 1
 
-    send_starts(players)
-    '''
+    mafs, coms, goods = send_starts(players)
+    
     comissaire_access = True
 
     while 1:
 
-        players, update_id         = incoming(update_id, players, phonebook, places)
+        players, update_id         = incoming(update_id, players)
 
         players, comissaire_access = check_mafs_murder (players, mafs, comissaire_access)
 
@@ -125,4 +139,4 @@ def game(players):
 
         check_alive (players, mafs, goods)
     
-        time.sleep(6)'''
+        time.sleep(6)
