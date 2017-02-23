37a60ba187690ca5d69a07a0c537183e74e925f7
Telpukhov <nofrost@inbox.ru>
2017-02-23 23:43:05 +0300

    More meat on bones

--- MyTests.py
+++ MyTests.py
@@ -22,10 +22,6 @@ class game(object):
             'cit_num':0,
             'doc_num':0,
             'com_num':0,
-            'mafs':[],
-            'cits':[],
-            'docs':[],
-            'coms':[]
         }
         self.__round_time__ = 5
     def add_player(self, adr):
@@ -34,6 +30,7 @@ class game(object):
                                         'name': 'Player_' + str(self.__player_num__+1),
                                         'alive': 1,
                                         'pointer': 0,
+                                        'acted': 0,
                                         'role': None
                                       }
         self.__player_num__ += 1
@@ -102,8 +99,11 @@ class game(object):
         return 0
 
     def reload_abilities(self):
-        None
-    def action(self, ):
+        for i in self.__players__:
+            self.__players__[i]['acted'] = 0
+
+    # функция обновления игры
+    def refresh(self, ):
         None
 
 def msg_handler(adr, msg):
@@ -119,13 +119,45 @@ def msg_handler(adr, msg):
         games_list.append((game_id))
         bot.sendMessage(adr, 'game ' + str(game_id) + ' starting')
 
-    if (msg == '/go'):  # Хостится игра
-        if adr == g.__host__:
-            # Сюда проверки на укомплектованность
-            g.__started__ = 1
-            g.__current_round__ = 1
-            g.say(g.everybody(), 'GAME IS STARTED')
-            g.say(g.everybody(), 'ROUND 1')
+    if (msg == '/go'):  # Комплектуется и запускается игра
+        if ('g' in globals()):
+            if adr == g.__host__:
+                # Укомплектовка состава
+                if (len(g.__players__) >= g.__roles__['maf_num'] + g.__roles__['doc_num'] + g.__roles__['com_num']):
+                    everybody = [i+1 for i in range(len(g.__players__))]
+                    mafs = random.sample(everybody, g.__roles__['maf_num'])
+                    rest = [i for i in everybody if i not in mafs]
+                    docs = random.sample(rest, g.__roles__['doc_num'])
+                    rest = [i for i in rest if i not in docs]
+                    coms = random.sample(rest, g.__roles__['com_num'])
+                    cits = [i for i in rest if i not in coms]
+                    g.__roles__['cit_num'] = len(cits)
+                    # присвоение ролей
+                    for i in mafs:
+                        g.__players__[i]['role'] = 'maf'
+                    for i in docs:
+                        g.__players__[i]['role'] = 'doc'
+                    for i in coms:
+                        g.__players__[i]['role'] = 'com'
+                    for i in cits:
+                        g.__players__[i]['role'] = 'cit'
+                    # Объявление ролей
+                    g.say(g.mafs(), 'YOU ARE MAFIA')
+                    g.say(g.cits(), 'YOU ARE CITIZEN')
+                    g.say(g.coms(), 'YOU ARE COMMISAIRE')
+                    g.say(g.docs(), 'YOU ARE DOCTOR')
+                    # Знакомство мафии
+                    maf_list_msg = ''
+                    for player_id in g.__players__:
+                        if g.__players__[player_id]['role'] == 'maf':
+                            maf_list_msg += str(player_id) + ' - ' + g.__players__[player_id]['name'] + ' \n'
+                    g.say(g.mafs(), 'YOUR COLLEAGUES:')
+                    g.say(g.mafs(), maf_list_msg)
+                    # Начало игры
+                    g.__started__ = 1
+                    g.__current_round__ = 1
+                    g.say(g.everybody(), 'GAME IS STARTED')
+                    g.say(g.everybody(), 'ROUND 1')
 
     if (msg == '/game_list'):  # Проверка списка игр
         bot.sendMessage(adr, str(games_list))
@@ -142,6 +174,20 @@ def msg_handler(adr, msg):
         if ('g' in globals()):
             g.countdown(3, 1, g.everybody())
 
+    m = re.search('/mafs ', msg) # Число мафов
+    if (m != None):
+        maf_num = int(msg[m.span()[1]: ])
+        g.__roles__['maf_num'] = maf_num
+
+    m = re.search('/docs ', msg) # Число докторов
+    if (m != None):
+        doc_num = int(msg[m.span()[1]: ])
+        g.__roles__['doc_num'] = doc_num
+
+    m = re.search('/coms ', msg) # Число коммисаров
+    if (m != None):
+        com_num = int(msg[m.span()[1]: ])
+        g.__roles__['com_num'] = com_num
 
 
     m = re.search('/change_name ', msg)  # Смена имени
@@ -200,6 +246,7 @@ time_stamp = 0
 # Basic loop
 while 1:
     time_stamp = time_handler(time_stamp)
+    print(time_stamp)
     pkgs = bot.getUpdates(offset = offset)
     if pkgs != []: # сообщение/я пришло... обрабатываем
         msg_num = len(pkgs) # число пришедших пакетов
@@ -208,4 +255,18 @@ while 1:
             msg = pkg['message']['text']
             msg_handler(adr, msg)
         offset = int(pkgs[-1].get('update_id')) + 1
+
+    # проверка на окончание кона
+    # в случае если все все сделали свои дела
+    cont = 0
+    if('g' in globals()):
+        if g.__started__:
+            for i in g.__players__:
+                if (g.__players__[i]['acted'] == 0):
+                    cont = 1
+            if ~cont:
+                time_stamp = g.__round_time__*60-1
+
+
+
     time.sleep(1)
